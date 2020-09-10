fda5039a6a2834e23c188db153eddcdeeafb15da
Drualus <kseniiavlasova@gmail.com>
2020-09-10 16:27:48 +0300

    Possible to click on building
    Only phantomic building will light up red

--- src/components/MapBG.js
+++ src/components/MapBG.js
@@ -6,9 +6,20 @@ import "react-leaflet-markercluster/dist/styles.min.css";
 import buildingsPolygon from "../building_polygon.json";
 import polygon_data from "./polygon_data.json";
 import Contex from "../store/context";
+import kgis_upe from '../data/kgis_upe.json';
 import L from "leaflet";
 
 delete L.Icon.Default.prototype._getIconUrl;
+
+const GetBalanceGroup = (kgisId) => {
+
+  if(kgisId !== ''){
+    var key =  kgis_upe.find((element)=> { return element.kgis_id === kgisId});
+
+    console.log(key.upe_id);
+
+  }
+}
 
 const PhantomicBuilding = (kgisId) => {
   var temp;
@@ -27,11 +38,20 @@ const PhantomicBuilding = (kgisId) => {
       return <GeoJSON key={kgisId} data={temp} style={style} />;
 };
 
+const NonePhantomicBuilding = (kgisId) => {
+
+  GetBalanceGroup(kgisId);
+
+  return(null);
+}
+
 const GeneralMap = () => {
   const { state, globalState } = useContext(Contex);
+  const { globalDispach } = useContext(Contex);
 
 
-  const position = [60.04220088616337, 30.339628111235275];
+
+  const position = [60.043048244264, 30.329257784423913];
 
   const style = {
     fillColor: "rgba(37, 47, 74, 0.24)",
@@ -53,22 +73,30 @@ const GeneralMap = () => {
     height: "100vh",
   };
 
+  const handleClick = (event) =>{
+      globalDispach({
+        type: "FILTERCOMPONENT",
+        bi_value: event.sourceTarget.feature.properties.kgisId,
+        isPhantomic: event.sourceTarget.feature.properties.isPhantomic,
+      });
+  }
+
   return (
     <Map
       className="markercluster-map"
       center={position}
-      zoom={20}
+      zoom={17}
       style={mapStyle}
     >
       <TileLayer
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <GeoJSON key={"building_polygons"} data={buildingsPolygon} />
+      <GeoJSON key={"building_polygons"} data={buildingsPolygon} onClick={handleClick} />
 
-      {globalState.isPhantomic && globalState.bi_value !== '' && typeof globalState.bi_value !== 'undefined'
+      {globalState.isPhantomic && (globalState.bi_value !== '' && typeof globalState.bi_value !== 'undefined')
         ? PhantomicBuilding(globalState.bi_value)
-        : null}
+        : NonePhantomicBuilding(globalState.bi_value)}
     </Map>
   );
 };
