58907021c23eb68933e395d286b80b75f8d7a4c3
Nikita Volynkin <nikita.volynkin@vaimo.com>
2020-09-18 02:11:48 +0300

    Home elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,16 @@ import { withRouter } from 'react-router-dom';
 import clsx from 'clsx';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import MapOutlinedIcon from '@material-ui/icons/MapOutlined';
+import WorkOutlineOutlinedIcon from '@material-ui/icons/WorkOutlineOutlined';
+import { Container, Grid, Paper, FormControl, InputLabel, Select, MenuItem, Box, Typography } from "@material-ui/core";
+import pieChart from '../img/pie-chart.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
-    padding: '24px'
+    padding: '24px',
+    width: '100%'
   },
   buttonContainer: {
     display: 'flex',
@@ -24,13 +28,13 @@ const useStyles = makeStyles((theme) => ({
     padding: '9px 14px',
     border: 'none',
     color: '#FFF',
-    width: '175px',
+    width: '100%',
     height: '142px',
     borderRadius: '4px',
     cursor: 'pointer',
     boxShadow: '4px 6px 18px rgba(0, 0, 0, 0.06)',
   },
-  buttonBalance : {
+  buttonBalance: {
     background: 'linear-gradient(127.52deg, #00CAFF 20.68%, #4A9CFF 80.9%)',
     marginRight: '24px',
   },
@@ -47,12 +51,48 @@ const useStyles = makeStyles((theme) => ({
     position: 'absolute',
     top: '15px',
     left: '14px',
+  },
+  pieChartPaper: {
+    padding: '30px'
+  },
+  pieCharts: {
+    display: 'flex'
+  },
+  rightPieChart: {
+    marginLeft: '12px'
+  },
+  leftPieChart: {
+    marginRight: '12px'
+  },
+  pieChartImage: {
+    width: '100%'
+  },
+  boxPaper: {
+    display: 'flex'
+  },
+  buttonTasks: {
+    background: 'linear-gradient(127.52deg, #00CAFF 20.68%, #4A9CFF 80.9%);',
+    marginRight: '24px'
   }
 }));
 
 
 export default function Home() {
   const classes = useStyles();
+  
+  const ButtonTasks = withRouter(({ history }) => (
+    <button
+      type='button'
+      className={clsx(classes.button, classes.buttonTasks)}
+      onClick={() => { history.push('/home') }}
+    >
+      <WorkOutlineOutlinedIcon className={classes.icon} />
+      <div className={classes.buttonContainer}>
+        <span className={classes.buttonText}>Страница</span>
+        <span className={classes.buttonText}>заданий</span>
+      </div>
+    </button>
+  ))
 
   const ButtonBalance = withRouter(({ history }) => (
     <button
@@ -74,7 +114,7 @@ export default function Home() {
       className={clsx(classes.button, classes.ButtonBuBd)}
       onClick={() => { history.push('/bubd') }}
     >
-      <MapOutlinedIcon className={classes.icon} /> 
+      <MapOutlinedIcon className={classes.icon} />
       <div className={classes.buttonContainer}>
         <span className={classes.buttonText}>Карта</span>
         <span className={classes.buttonText}>БУ/БД</span>
@@ -84,8 +124,23 @@ export default function Home() {
 
   return (
     <div className={classes.root}>
-      <ButtonBalance />
-      <ButtonBuBd />
+      <Grid container spacing={3}>
+        <Grid item xs={6} spacing={3} className={classes.pieCharts}>
+          <Grid item xs={6}>
+            <Paper className={clsx(classes.pieChartPaper, classes.leftPieChart)}><img src={pieChart} className={classes.pieChartImage} /></Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={clsx(classes.pieChartPaper, classes.rightPieChart)}><img src={pieChart} className={classes.pieChartImage} /></Paper>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Box className={classes.boxPaper}>
+            <ButtonTasks />
+            <ButtonBalance />
+            <ButtonBuBd />
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 }
